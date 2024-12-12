19367abec1966748efd11da0cd48a125fe703506
hoangtusushi <127019167+hoangtusushi@users.noreply.github.com>
2024-12-12 18:31:09 +0700

    OnBoarding Page

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:shopify/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:shopify/utils/constants/text_strings.dart';
+import 'package:shopify/utils/theme/theme.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: TTexts.appName,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      home: const OnBoardingScreen(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,8 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:shopify/utils/constants/text_strings.dart';
 import 'package:shopify/utils/theme/theme.dart';
 
+import 'app.dart';
+
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: TTexts.appName,
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-      debugShowCheckedModeBanner: false,
-      // initialBinding: GeneralBindings(),
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'Awesome! 🎊 Project Structure is set up and running. 🎊',
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
-    );
-  }
 }
